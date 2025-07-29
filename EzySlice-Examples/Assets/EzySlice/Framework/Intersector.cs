f38421da59f138a0107d91b4ebc0c629a3ae7a72
ZaoTX <568146965hzy@gmail.com>
2025-07-29 11:07:47 +0200

    update, move the project to new pc

--- EzySlice-Examples/Assets/EzySlice/Framework/Intersector.cs
+++ EzySlice-Examples/Assets/EzySlice/Framework/Intersector.cs
@@ -45,8 +45,9 @@ namespace EzySlice {
         /**
          * Support functionality 
          */
-        public static float TriArea2D(float x1, float y1, float x2, float y2, float x3, float y3) {
-            return (x1 - x2) * (y2 - y3) - (x2 - x3) * (y1 - y2);
+        public static float TriArea2D(float x1, float y1, float x2, float y2, float x3, float y3)
+        {
+            return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
         }
 
         /**
